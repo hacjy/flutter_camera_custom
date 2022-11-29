71ba429f0e2ddb90bed4411f26aaa0e1e51170a6
hacjy <chejieyang@126.com>
2022-11-29 16:45:13 +0800

    添加说明

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:camera_demo/custom_camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
 
@@ -11,6 +16,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
